d22837fc188dbc422f488c693b72e0f4eab7aefe
qcollinscode <qcollins@qraynix.com>
2017-04-17 02:03:18 -0700

    corrected bug

--- fizzbuzz/Program.cs
+++ fizzbuzz/Program.cs
@@ -10,12 +10,30 @@ namespace fizzbuzz
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Please enter 1 to 100");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
             string str = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.ResetColor();
+
             int num = Int32.Parse(str);
             StringBuilder result = new StringBuilder("");
 
-            Console.WriteLine("\n");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine(" " + "Results");
+            Console.WriteLine("---------------------------------------- \n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(" " + "Multiples of 3 = Fizz \n");
+            Console.WriteLine(" " + "Multiples of 5 = Buzz \n");
+            Console.WriteLine(" " + "Multiples of 3 and 5 = FizzBuzz \n");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Magenta;
             for (int i = 0; i <= num; i++)
             {
                 if (i % 3 == 0 && i % 5 == 0)
@@ -30,6 +48,8 @@ namespace fizzbuzz
                 }
             }
             Console.WriteLine(result);
+            Console.ResetColor();
+            Console.ReadKey();
         }
     }
 }
